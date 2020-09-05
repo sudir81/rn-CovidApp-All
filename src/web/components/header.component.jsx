606abdb31ea554f5c0d65846a7675f351b0c2ecb
sudir81 <papinenisudheer@yahoo.com>
2020-09-05 03:18:49 -0400

    firebase deploy
    added image & firebase details

--- src/web/components/header.component.jsx
+++ src/web/components/header.component.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './header.css';
 import LanguageIcon from '@material-ui/icons/Language';
 import {Avatar} from '@material-ui/core';
+import covidImg from '../../images/covid.png';
 
 const Header = () => {
   return (
     <div className="header">
-      <img
-        src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png"
-        alt="COVID-19"
-        className="header_icon"
-      />
+      <img src={covidImg} alt="COVID-19" className="header_icon" />
       <div className="header_right">
         <LanguageIcon />
         <Avatar />
